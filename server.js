26037fc7befa0e896e238442cddee6d1dbc24dc8
Kyryl Kolisnichenko <kolis198535@gmail.com>
2025-05-26 16:13:29 +0300

    Add basic 404 route and error-handling middleware

--- server.js
+++ server.js
@@ -144,6 +144,19 @@ app.get("/summary", (req, res) => {
     res.json(summary);
 });
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Not Found" });
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    res.status(500).json({
+        error: "Internal Server Error",
+        message: err.message,
+    });
+});
+
 app.listen(3000, () => {
     console.log("http://localhost:3000/");
 });
